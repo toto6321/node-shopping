65d590c612a33c3401fcf87947718ae5395b89d4
toto <toto6321@outlook.com>
2018-11-17 23:50:28 -0500

    FEATURE: add statement for creating database node_shopping and table account

--- node_shopping.sql
+++ node_shopping.sql
@@ -1,23 +1,26 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+CREATE DATABASE `node_shopping` CHARSET=utf8;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
+USE `node_shopping`;
 
 CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(200) NOT NULL,
   `description` text NOT NULL,
   `price` double NOT NULL,
   `date` date NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '1'
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  primary key(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `account` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`email` varchar(50) NOT NULL,
+    `password` varchar(100) NOT NULL,
+     PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `products` (`id`, `name`, `description`, `price`, `date`, `status`) VALUES
-(1, 'Odio commodi', 'Aliquid animi dolorem a minima et alias ea optio illo. Laboriosam est est velit aut asperiores', 44.5, '2016-09-15', 1),
+(1, 'Odio commodi', productsproductsproductsaccountaccount'Aliquid animi dolorem a minima et alias ea optio illo. Laboriosam est est velit aut asperiores', 44.5, '2016-09-15', 1),
 (2, 'Iusto impedit', 'Nemo voluptas laboriosam repellendus hic dolore minima in rerum. Totam qui itaque quia sit sed eaque', 785.24, '2016-09-29', 1),
 (3, 'Ut sunt et', 'Odit facere dolor doloremque neque similique ducimus beatae aut. Et molestiae aspernatur sint odio vel ut reprehenderit', 78, '2016-09-08', 1),
 (4, 'Alias hic repellendus', 'Quod voluptatem sunt voluptate. Incidunt neque exercitationem quia. Vitae repellendus quia harum repellendus', 57, '2016-09-14', 1),
@@ -27,14 +30,3 @@ INSERT INTO `products` (`id`, `name`, `description`, `price`, `date`, `status`) 
 (8, 'Cumque architecto', 'Dolorem et repellat sed velit veritatis delectus. Deleniti eveniet voluptatum natus ut ullam. Repellat consequatur non eligendi officiis eum veniam', 24, '2016-09-24', 1),
 (9, 'Aut commodi occaecati', 'Repudiandae nihil sit incidunt. Qui minima maxime quibusdam. Quod doloribus accusantium voluptatem beatae enim nisi architecto. Harum rem veniam voluptatum', 32, '2016-09-16', 1),
 (10, 'Eos modi', 'Veritatis similique est ut iusto sint. Rerum natus dolorem ipsa soluta voluptas sed et iste. Officiis est modi. Optio facilis sint commodi veritatis', 643, '2016-09-15', 1);
-
-
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
-
-
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
