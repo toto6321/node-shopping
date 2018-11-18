251965a639814b85d347da25b6b922de5411cccc
toto <toto6321@outlook.com>
2018-11-18 01:24:30 -0500

    FEATURE: add basic login and sign up views and routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,128 @@
 var express = require('express');
+var session = require('express-session');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var conn = require('./connection');
+var connection = conn.connection();
+
+router.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+
+
+
+// login page
+router.get('/', function (req, res, next) {
+    res.render('login', {});
 });
+
+
+router.get('/login', function (req, res, next) {
+    res.render('login', {});
+});
+
+// sign upo page
+router.get('/signup', function (req, res, next) {
+    res.render('signup', {});
+});
+
+
+router.post('/login', function (req, res, next) {
+    let email = req.body.email;
+    let password = req.body.password;
+    connection.query(`SELECT password FROM node_shopping.account where email = '${email}'`, function (err, rows) {
+        if (err) {
+            res.status(500);
+            res.json({
+                error: {
+                    code: '11000',
+                    message: 'internal error!'
+                }
+            });
+            throw err;
+        } else {
+            console.log(rows);
+            if (rows.length === 0) {
+                res.json({
+                    error: {
+                        code: '10001',
+                        message: 'The user does not exists!'
+                    }
+                })
+            } else if (password === rows[0].password) {
+                req.session.email = email;
+                res.redirect('../');
+            } else {
+                res.json({
+                    error: {
+                        code: '10010',
+                        message: 'The password does not match!'
+                    }
+                })
+            }
+        }
+
+    });
+});
+
+router.post('/signup', function (req, res, next) {
+    let email = req.body.email;
+    let password = req.body.password;
+    let password2 = req.body.password2;
+    if (password !== password2) {
+        res.json({
+            error: {
+                code: '11000',
+                message: 'Passwords do not match'
+            }
+        });
+
+    }
+    const select = `SELECT password FROM \`node_shopping\`.\`account\` where email = '${email}'`;
+    console.log(select);
+    connection.query(select, function (err, rows) {
+        if (err) {
+            res.status(500);
+            res.json({
+                error: {
+                    code: '11000',
+                    message: 'internal error!'
+                }
+            });
+            throw err;
+        } else {
+            console.log(rows);
+            if (rows.length > 0) {
+                res.json({
+                    error: {
+                        code: '10100',
+                        message: 'The user has already existed!'
+                    }
+                })
+            } else {
+                const insert = `INSERT INTO \`node_shopping\`.\`account\`(\`email\`, \`password\`) VALUES ('${email}', '${password}')`;
+                console.log(insert);
+                connection.query(insert, function (err, rows) {
+                    if (err) {
+                        res.status(500);
+                        res.json({
+                            error: {
+                                code: '11000',
+                                message: 'internal error!'
+                            }
+                        });
+                    } else{
+                        res.redirect('../');
+                    }
+                });
+
+            }
+        }
+
+    });
+});
+
 
 module.exports = router;
